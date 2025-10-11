42629974b7c1e1b881d0ca4507ecc56cfd1033f4
djordjemoj <djordjemojsic2001@gmail.com>
2025-10-11 16:15:04 +0200

    feat - more random seed data - users - books

--- src/database/factories/BookFactory.php
+++ src/database/factories/BookFactory.php
@@ -16,11 +16,42 @@ class BookFactory extends Factory
      */
     public function definition(): array
     {
+        $serbianBooks = [
+            'Na Drini ćuprija', 'Prokleta avlija', 'Travnička hronika', 'Gospođica', 'Jelena, žena koje nema',
+            'Gorski vijenac', 'Luca mikrokozma', 'Pisma iz Italije', 'Ogledalo srpsko', 'Naučnik i pesnik',
+            'Krvava bajka', 'Krv i pepeo Jasenovca', 'Čovek po imenu Uve', 'Sto godina samoće', 'Ana Karenjina',
+            'Rat i mir', 'Zločin i kazna', 'Braća Karamazovi', 'Idiot', 'Majstor i Margarita',
+            'Doktor Živago', 'Lolita', '1984', 'Životinjska farma', 'Veliki Getsbi',
+            'Ubiti pticu rugalicu', 'Gronovi gneva', 'Stranac', 'Kuga', 'Mali princ',
+            'Zapad od raja', 'Hiljadu i jedna noć', 'Don Kihot', 'Hamlet', 'Romeo i Julija',
+            'Makbet', 'Othello', 'Kralj Lir', 'Midsummer Night\'s Dream', 'The Tempest',
+            'Pride and Prejudice', 'Jane Eyre', 'Wuthering Heights', 'Great Expectations', 'Oliver Twist',
+            'David Copperfield', 'A Tale of Two Cities', 'Les Misérables', 'The Hunchback of Notre-Dame',
+            'The Count of Monte Cristo', 'The Three Musketeers', 'Around the World in Eighty Days'
+        ];
+
+        $serbianAuthors = [
+            'Ivo Andrić', 'Petar II Petrović Njegoš', 'Miloš Crnjanski', 'Meša Selimović', 'Danilo Kiš',
+            'Milorad Pavić', 'Isidora Sekulić', 'Branislav Nušić', 'Jovan Sterija Popović', 'Laza Kostić',
+            'Jovan Jovanović Zmaj', 'Đura Jakšić', 'Laza Lazarević', 'Stevan Sremac', 'Borisav Stanković',
+            'Radoje Domanović', 'Veljko Petrović', 'Momo Kapor', 'David Albahari', 'Goran Petrović',
+            'Vladimir Arsenijević', 'Svetislav Basara', 'Radoslav Petković', 'Aleksandar Gatalica',
+            'Vladimir Pištalo', 'Dragan Velikić', 'Milica Mićić Dimovska', 'Svetlana Velmar-Janković',
+            'Grozdana Olujić', 'Ljiljana Habjanović Đurović'
+        ];
+
+        $genres = [
+            'Fiction', 'Drama', 'Poetry', 'History', 'Biography', 'Science Fiction',
+            'Mystery', 'Romance', 'Thriller', 'Fantasy', 'Adventure', 'Philosophy',
+            'Psychology', 'Sociology', 'Politics', 'Economics', 'Art', 'Music',
+            'Travel', 'Cooking', 'Health', 'Education', 'Religion', 'Sports'
+        ];
+
         return [
-            'title' => $this->faker->sentence(3),
-            'author' => $this->faker->name(),
-            'year' => $this->faker->numberBetween(1900, 2024),
-            'genre' => $this->faker->randomElement(['Fiction', 'Non-fiction', 'Drama', 'Poetry', 'Science', 'History']),
+            'title' => $this->faker->randomElement($serbianBooks),
+            'author' => $this->faker->randomElement($serbianAuthors),
+            'year' => $this->faker->numberBetween(1800, 2024),
+            'genre' => $this->faker->randomElement($genres),
             'isbn' => $this->faker->unique()->isbn13(),
         ];
     }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,43 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        if (!User::where('email', 'admin@local.test')->exists()) {
+            User::factory()->admin()->create([
+                'name' => 'Admin User',
+                'email' => 'admin@local.test',
+                'password' => bcrypt('password'),
+            ]);
+        }
 
         if (!User::where('email', 'test@example.com')->exists()) {
             User::factory()->create([
                 'name' => 'Test User',
                 'email' => 'test@example.com',
+                'password' => bcrypt('password'),
             ]);
         }
 
-        if (!Book::where('isbn', '978-86-17-12345-6')->exists()) {
-            Book::factory()->create([
-                'title' => 'Na Drini ćuprija',
-                'author' => 'Ivo Andrić',
-                'year' => 1945,
-                'genre' => 'Fiction',
-                'isbn' => '978-86-17-12345-6'
-            ]);
-        }
+        User::factory(8)->create();
 
-        if (!Book::where('isbn', '978-86-17-12346-3')->exists()) {
-            Book::factory()->create([
-                'title' => 'Gorski vijenac',
-                'author' => 'Petar II Petrović Njegoš',
-                'year' => 1847,
-                'genre' => 'Poetry',
-                'isbn' => '978-86-17-12346-3'
-            ]);
-        }
-
-        if (!Book::where('isbn', '978-86-17-12347-0')->exists()) {
-            Book::factory()->create([
-                'title' => 'Prokleta avlija',
-                'author' => 'Ivo Andrić',
-                'year' => 1954,
-                'genre' => 'Fiction',
-                'isbn' => '978-86-17-12347-0'
-            ]);
-        }
+        Book::factory(35)->create();
     }
 }
